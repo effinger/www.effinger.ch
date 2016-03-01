44e3f5d0211fe090fdf731e1bd7787c365bd1c75
marcojakob <majakob@gmx.ch>
2016-03-01 21:45:04 +0100

    Remove JavaScript side panel menu (we only use the classic menu at the top)

--- static/js/all.js
+++ static/js/all.js
@@ -27,7 +27,6 @@
         
         $(window).trigger("resize");            
         init_classic_menu();
-        init_side_panel();
         init_lightbox();
         init_parallax();
         init_shortcodes();
@@ -44,7 +43,6 @@
     $(window).resize(function(){
         
         init_classic_menu_resize();
-        init_side_panel_resize()
         js_height_init();
         
     });
@@ -695,119 +693,6 @@ function initPageSliders(){
     })(jQuery);
 };
 
-
-/* ---------------------------------------------
-     Side panel
-   --------------------------------------------- */
-    
-    var side_panel = $(".side-panel");
-    var sp_button = $(".sp-button");
-    var sp_close_button = $(".sp-close-button");
-    var sp_overlay = $(".sp-overlay");
-    
-    function sp_panel_close(){
-        side_panel.animate({
-            opacity: 0,
-            left: -270
-        }, 500, "easeOutExpo");
-        sp_overlay.fadeOut();
-        
-        
-        if ($(".owl-carousel").lenth) {
-            $(".owl-carousel").data("owlCarousel").play();
-        }
-    }
-    
-    function init_side_panel(){
-        (function($){
-            "use strict";
-            
-            sp_button.click(function(){
-            
-                side_panel.animate({
-                    opacity: 1,
-                    left: 0
-                }, 500, "easeOutExpo");
-                
-                setTimeout(function(){
-                    sp_overlay.fadeIn();
-                }, 100);
-                
-                if ($(".owl-carousel").lenth) {
-                    $(".owl-carousel").data("owlCarousel").stop();
-                }
-                
-                return false;
-            
-            });
-            
-            sp_close_button.click(function(){
-                sp_panel_close();
-                return false;
-            });
-            sp_overlay.click(function(){
-                sp_panel_close();
-                return false;
-            });
-            
-            $("#side-panel-menu").find("a:not(.sp-has-sub)").click(function(){
-                if (!($(window).width() >= 1199)) {
-                    sp_panel_close();
-                }
-            });
-            
-            
-            // Sub menu
-        
-            var spHasSub = $(".sp-has-sub");
-            var spThisLi;
-            
-            spHasSub.click(function(){
-            
-                spThisLi = $(this).parent("li:first");
-                if (spThisLi.hasClass("js-opened")) {
-                    spThisLi.find(".sp-sub:first").slideUp(function(){
-                        spThisLi.removeClass("js-opened");
-                        spThisLi.find(".sp-has-sub").find(".fa:first").removeClass("fa-angle-up").addClass("fa-angle-down");
-                    });
-                }
-                else {
-                    $(this).find(".fa:first").removeClass("fa-angle-down").addClass("fa-angle-up");
-                    spThisLi.addClass("js-opened");
-                    spThisLi.find(".sp-sub:first").slideDown();
-                }
-                
-                return false;
-                
-            });
-            
-        })(jQuery);
-    }
-    
-    function init_side_panel_resize(){
-        (function($){
-            "use strict";
-            
-             if ($(window).width() >= 1199){
-               side_panel.css({
-                   opacity: 1,
-                   left: 0
-               });
-               $(".side-panel-is-left").css("margin-left", "270px");
-               sp_button.css("display", "none");
-               sp_close_button.css("display", "none");
-             } else {
-                 side_panel.css({
-                     opacity: 0,
-                     left: -270
-                 });
-                 $(".side-panel-is-left").css("margin-left", "0");
-                 sp_button.css("display", "block");
-                 sp_close_button.css("display", "block");
-             }
-            
-        })(jQuery);
-    }
 
 /* ---------------------------------------------
  Portfolio section
