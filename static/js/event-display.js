56a63569f44546fce9983fe36b49345c6f06ce66
Raffael Krebs <raffael.krebs@protonmail.ch>
2023-09-11 15:00:12 +0200

    Ditch semicolons in event-display.js

--- static/js/event-display.js
+++ static/js/event-display.js
@@ -1,85 +1,87 @@
-const urlParams = getUrlParams();
-const theme = urlParams['theme'];
-const align = urlParams['align'];
-const size = urlParams['size'];
-const keystone = parseFloat(urlParams['keystone']);
-const tzdelta = parseInt(urlParams['timezone']);
+const urlParams = getUrlParams()
+const theme = urlParams['theme']
+const align = urlParams['align']
+const size = urlParams['size']
+const keystone = parseFloat(urlParams['keystone'])
+const tzdelta = parseInt(urlParams['timezone'])
 
 $(document).ready(function () {
-  initializeDisplay();
+  initializeDisplay()
 
   // Load and periodically update events.
-  loadEvents();
-  setInterval(loadEvents, 20000);
-});
+  loadEvents()
+  setInterval(loadEvents, 20000)
+})
 
 function initializeDisplay() {
-  moment.locale('de');
+  moment.locale('de')
 
   // Color theme
   if (theme === 'black') {
-    $('body').addClass('theme-black');
+    $('body').addClass('theme-black')
   }
 
   // Vertical alignment.
   if (align) {
-    $('.container').css('height', 'auto');
+    $('.container').css('height', 'auto')
     if (align === 'bottom') {
-      $('.container').css('bottom', '0');
+      $('.container').css('bottom', '0')
     }
   }
 
   // Size.
   if (size === 'large') {
-    $('body').addClass('size-large');
+    $('body').addClass('size-large')
   }
 
   // Keystone correction.
   if (keystone) {
-    $('.container-outer').css('left', `${Math.abs(keystone)}px'`);
-    $('.container-outer').css('right', `${Math.abs(keystone)}px'`);
-    $('.container-outer').css('transform', `perspective(5000px) rotateX(${keystone}deg)`);
-    $('.container-outer').css('height', `${100 - keystone / 10}%`);
+    $('.container-outer')
+        .css('left', `${Math.abs(keystone)}px'`)
+        .css('right', `${Math.abs(keystone)}px'`)
+        .css('transform', `perspective(5000px) rotateX(${keystone}deg)`)
+        .css('height', `${100 - keystone / 10}%`)
   }
 }
 
 function loadEvents() {
   // Show loading indicator.
-  $('.logo-container').addClass('loading');
+  $('.logo-container').addClass('loading')
 
-  const startOfDay = moment().startOf('day');
-  const endOfDay = moment().endOf('day');
+  const startOfDay = moment().startOf('day')
+  const endOfDay = moment().endOf('day')
 
   // Set title date of today.
-  $('.title').html(`Gäste Heute &mdash; ${startOfDay.format('dd DD.MM.YYYY')}`);
+  $('.title').html(`Gäste Heute &mdash; ${startOfDay.format('dd DD.MM.YYYY')}`)
 
   $.ajax({
     url: window.BENJIBOOKS_API_URL,
     success: function(data) {
-      const events = data.map(parseEvent);
+      const events = data.map(parseEvent)
 
-      // Sort by start time.
       events.sort(function(a, b) {
-        return a.start - b.start;
-      });
+        return a.start - b.start
+      })
 
       // Display events in table if we have events.
       if (events.length > 0) {
-        $('.center-content').html(eventsTable(events));
+        $('.center-content').html(eventsTable(events))
       } else {
         // Empty table.
-        $('.center-content').html('Extra Raum gewünscht? Gastgeber fragen oder spontan reservieren unter effinger.ch/raeume');
+        $('.center-content').html(
+          '<p>Extra Raum gewünscht? Gastgeber fragen oder spontan reservieren unter effinger.ch/raeume</p>'
+        )
       }
 
       // Hide loading indicator.
-      $('.logo-container').removeClass('loading');
+      $('.logo-container').removeClass('loading')
     },
     error: function(err) {
-      console.log(this.url);
-      console.log(`AJAX error in request: ${JSON.stringify(err, null, 2)}`);
+      console.log(this.url)
+      console.log(`AJAX error in request: ${JSON.stringify(err, null, 2)}`)
     },
     timeout: 12000,
-  });
+  })
 }
 
 function parseEvent(eventData) {
@@ -95,37 +97,37 @@ function parseEvent(eventData) {
     // Convert dates
     start: moment(eventData.start),
     end: moment(eventData.end),
-  };
+  }
 
   // Adjust timezone
   if (typeof tzdelta !== 'undefined' && tzdelta) {
-    event.start = event.start.add(tzdelta, 'hour');
-    event.end = event.end.add(tzdelta, 'hour');
+    event.start = event.start.add(tzdelta, 'hour')
+    event.end = event.end.add(tzdelta, 'hour')
 
     // Detect Daylight Savings Time
   } else if (event.start.isDST()) {
-    event.start = event.start.add(1, 'hour');
-    event.end = event.end.add(1, 'hour');
+    event.start = event.start.add(1, 'hour')
+    event.end = event.end.add(1, 'hour')
   }
 
-  return event;
+  return event
 }
 
 function eventsTable(events) {
   return `
     <table class="table room-table">
     <tbody>
-      ${events.map(eventRow).join()}
+      ${events.map(eventRow).join("")}
     </tbody>
     </table>
-  `;
+  `
 }
 
 function eventRow(event) {
+  const rowClasses = ['event']
+  if (event.end.isBefore()) rowClasses.push('ended')
+  if (isBrownbag(event)) rowClasses.push('brownbag')
 
-  const rowClasses = []
-  if (event.end.isBefore()) rowClasses.push('event-ended');
-  if (isBrownbag(event)) rowClasses.push('brownbag');
   return `
    <tr class="${rowClasses.join(' ')}">
      <td class="event-time">
@@ -140,7 +142,7 @@ function eventRow(event) {
        <div class="room-name">${event.roomName}</div>
      </td>
    </tr>
-  `;
+  `
 }
 
 function isBrownbag(event) {
@@ -149,11 +151,11 @@ function isBrownbag(event) {
 
 // Read a page's GET URL variables and return them as a map.
 function getUrlParams() {
-  const params = {};
-  const urlParams = window.location.search.slice(1).split('&');
+  const params = {}
+  const urlParams = window.location.search.slice(1).split('&')
   for (let i = 0, kv; i < urlParams.length; i++) {
-    kv = urlParams[i].split('=');
-    params[kv[0]] = kv[1];
+    kv = urlParams[i].split('=')
+    params[kv[0]] = kv[1]
   }
-  return params;
+  return params
 }
