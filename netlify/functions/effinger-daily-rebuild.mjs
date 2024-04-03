0abe240840615a0f85a97a6703888e6270c88895
Raffael Krebs <raffael.krebs@protonmail.ch>
2024-04-03 17:29:00 +0200

    Add the node-fetch package

--- netlify/functions/effinger-daily-rebuild.mjs
+++ netlify/functions/effinger-daily-rebuild.mjs
@@ -1,5 +1,6 @@
 /*
- * See https://docs.netlify.com/functions/scheduled-functions/
+ * Execution logs: https://app.netlify.com/sites/effinger/logs/functions/effinger-daily-rebuild
+ * Netlify function docs: https://docs.netlify.com/functions/scheduled-functions/
  */
 
 import fetch from 'node-fetch'
@@ -7,7 +8,7 @@ import fetch from 'node-fetch'
 const BUILD_WEBHOOK = 'https://api.netlify.com/build_hooks/57f05f05d6865d2f243241ab'
 
 export default async (_request, _context) => {
-  await fetch(BUILD_WEBHOOK, {method: 'POST'}).then(
+  return await fetch(BUILD_WEBHOOK, {method: 'POST'}).then(
       (response) => {
         console.log('Build hook successful:', response)
         return new Response("ok")
